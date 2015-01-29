278e8eedbe09f3df6e77334c2eee86fadcac51fc
Mathias Buus <mathiasbuus@gmail.com>
2015-01-29 14:29:25 +0100

    split up tests

--- test/misc.js
+++ test/misc.js
@@ -1,5 +1,4 @@
 var tape = require('tape')
-var fs = require('fs')
 var cosmic = require('./fixtures/cosmic')
 var validator = require('../')
 var validatorRequire = require('../require')
@@ -186,24 +185,4 @@ tape('do not mutate schema', function(t) {
 
   t.same(sch, copy, 'did not mutate')
   t.end()
-})
-
-var files = fs.readdirSync(__dirname+'/json-schema-draft4')
-  .map(function(file) {
-    if (file === 'definitions.json') return null
-    if (file === 'refRemote.json') return null
-    return require('./json-schema-draft4/'+file)
-  })
-  .filter(Boolean)
-
-files.forEach(function(file) {
-  file.forEach(function(f) {
-    tape('json-schema-test-suite '+f.description, function(t) {
-      var validate = validator(f.schema)
-      f.tests.forEach(function(test) {
-        t.same(validate(test.data), test.valid, test.description)
-      })
-      t.end()
-    })
-  })
 })
