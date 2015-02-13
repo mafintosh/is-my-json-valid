a8c5ca06d8eff0ad6ad50722fa7d13d8c4c16df4
Daniel Juhl <danieljuhl@gmail.com>
2015-02-13 09:54:37 +0100

    Do not mutate scoped errors in verbose mode

--- index.js
+++ index.js
@@ -124,19 +124,18 @@ var compile = function(schema, cache, root, reporter, opts) {
 
     var indent = 0
     var error = function(msg) {
-      if (reporter === false) {
-        validate('errors++')
-        return
-      }
-
-      var n = gensym('error')
-      scope[n] = {field:formatName(name), message:msg}
       validate('errors++')
-      validate('if (validate.errors === null) validate.errors = []')
-      if (verbose) {
-        validate('%s.value = %s', n, name)
+      if (reporter === true) {
+        validate('if (validate.errors === null) validate.errors = []')
+        if (verbose) {
+          validate('validate.errors.push({field:"%s",message:"%s",value:%s})', formatName(name), msg, name)
+        }
+        else {
+          var n = gensym('error')
+          scope[n] = {field:formatName(name), message:msg}
+          validate('validate.errors.push(%s)', n)
+        }
       }
-      validate('validate.errors.push(%s)', n)
     }
 
     if (node.required === true) {
