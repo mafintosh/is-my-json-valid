9f465361a9e92402a84abaabc80c6b67d0533855
RGBboy <l-_-l@rgbboy.com>
2015-05-01 16:36:29 +1200

    Added index of array item to field

--- index.js
+++ index.js
@@ -30,8 +30,9 @@ var get = function(obj, additionalSchemas, ptr) {
 }
 
 var formatName = function(field) {
-  var pattern = /\[[^\[\]"]+\]/
-  while (pattern.test(field)) field = field.replace(pattern, '.*')
+  field = JSON.stringify(field)
+  var pattern = /\[([^\[\]"]+)\]/
+  while (pattern.test(field)) field = field.replace(pattern, '."+$1+"')
   return field
 }
 
@@ -132,11 +133,9 @@ var compile = function(schema, cache, root, reporter, opts) {
       if (reporter === true) {
         validate('if (validate.errors === null) validate.errors = []')
         if (verbose) {
-          validate('validate.errors.push({field:%s,message:%s,value:%s})', JSON.stringify(formatName(prop || name)), JSON.stringify(msg), value || name)
+          validate('validate.errors.push({field:%s,message:%s,value:%s})', formatName(prop || name), JSON.stringify(msg), value || name)
         } else {
-          var n = gensym('error')
-          scope[n] = {field:formatName(prop || name), message:msg}
-          validate('validate.errors.push(%s)', n)
+          validate('validate.errors.push({field:%s,message:%s})', formatName(prop || name), JSON.stringify(msg))
         }
       }
     }
